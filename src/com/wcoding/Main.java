7428dfc0327a777d02fc896a5d20608542fa3226
Farrukh <augini@inbox.ru>
2021-02-09 11:55:01 +0900

    feature: mortgage calculator (validate inputs)

--- src/com/wcoding/Main.java
+++ src/com/wcoding/Main.java
@@ -14,17 +14,54 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         //get principal
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+
+        // validate principal
+        while(true){
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+
+            if(principal >= 1000 && principal <= 1000000){
+                break;
+            } else {
+                System.out.println("Enter a number between 1,000 and 1,000,000");
+                continue;
+            }
+        }
 
         //get annual interest rate
-        System.out.print("Annual Interest Rate: ");
-        float annual_interest_rate = scanner.nextFloat();
+        float annual_interest_rate;
+
+        // validate annual interest rate
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            annual_interest_rate = scanner.nextFloat();
+
+            if(annual_interest_rate > 0 && annual_interest_rate <= 30){
+                break;
+            }else {
+                System.out.println("Enter a value greater than 0 and less than or equal to 30");
+                continue;
+            }
+        }
+
         float monthlyInterest = (annual_interest_rate / PERCENT) / MONTHS_IN_YEAR;
 
         //get period length
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
+        byte years;
+
+        // validate period length
+        while(true){
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+
+            if(years > 0 && years <= 30){
+                break;
+            }else{
+                System.out.println("Enter a value greater than 0 and less than or equal to 30");
+                continue;
+            }
+        }
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
         //calculate mortgage
