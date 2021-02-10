0cc00ef4fb975ce6dc009f3fe0a075680ae42359
Farrukh <augini@inbox.ru>
2021-02-10 18:53:51 +0900

    fix: some more features with java

--- src/com/wcoding/Main.java
+++ src/com/wcoding/Main.java
@@ -2,18 +2,24 @@ package com.wcoding;
 
 public class Main {
     public static void main(String[] args) {
+        TextBox textBox = new TextBox(true, "Hello");
 
-        int principal = (int) Console.readNumber("Principal ($1K - $1M): ", 1000, 1000000);
-        float annual_interest_rate = (float) Console.readNumber("Annual Interest Rate: ", 0, 30);
-        byte years = (byte) Console.readNumber("Period (Years): ",0,30);
+        Point point1 = new Point(1,2);
+        Point point2 = new Point(1,2);
+        System.out.println(point1.equals(point2));
 
-        MortgageCalculator calculator = new MortgageCalculator(principal, annual_interest_rate, years);
+//      int principal = (int) Console.readNumber("Principal ($1K - $1M): ", 1000, 1000000);
+//      float annual_interest_rate = (float) Console.readNumber("Annual Interest Rate: ", 0, 30);
+//      byte years = (byte) Console.readNumber("Period (Years): ",0,30);
+//
+//      MortgageCalculator calculator = new MortgageCalculator(principal, annual_interest_rate, years);
+//
+//      MortgageReport report = new MortgageReport(calculator);
+//      //display calculated mortgage rate
+//      report.printMortgage();
+//      //display monthly remaining payments
+//      report.printPaymentSchedule();
 
-        MortgageReport report = new MortgageReport(calculator);
-        //display calculated mortgage rate
-        report.printMortgage();
-        //display monthly remaining payments
-        report.printPaymentSchedule();
     }
 
 }
--- src/com/wcoding/TextBox.java
+++ src/com/wcoding/TextBox.java
@@ -1,7 +1,17 @@
 package com.wcoding;
 
-public class TextBox {
-    public String text = "";
+public class TextBox extends UIControl {
+    private String text = "";
+
+    public TextBox(boolean isEnabled, String text) {
+        super(isEnabled);
+        this.text = text;
+    }
+
+    @Override
+    public String toString(){
+        return this.text;
+    };
 
     public void setText(String text){
         this.text = text;
