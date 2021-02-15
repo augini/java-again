9f05050ac920073a77fe9d88a183310f9009e733
Farrukh <augini@inbox.ru>
2021-02-15 16:47:26 +0900

    fix: refactoring

---
+++ src/com/wcoding/collections/ListDemo.java
@@ -0,0 +1,10 @@
+package com.wcoding.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void show(){
+        List<String> list = new ArrayList<>();
+    }
+}
