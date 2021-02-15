8aa506ce808b4d8750c03c27dcecc37ea62c75f2
Farrukh <augini@inbox.ru>
2021-02-15 17:45:44 +0900

    feature: add comparator demo

--- src/com/wcoding/Main.java
+++ src/com/wcoding/Main.java
@@ -1,18 +1,31 @@
 package com.wcoding;
 
 import com.wcoding.collections.CollectionsDemo;
+import com.wcoding.collections.Customer;
+import com.wcoding.collections.EmailComparator;
+import com.wcoding.collections.ListDemo;
 import com.wcoding.generics.GenericList;
 import com.wcoding.generics.User;
 import com.wcoding.generics.Utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
-        CollectionsDemo.show();
+        List <Customer> customers = new ArrayList<>();
+
+        customers.add(new Customer("John", "e2"));
+        customers.add(new Customer("Doe", "e3"));
+        customers.add(new Customer("Alexis", "e1"));
+
+//        Collections.sort(customers);
+
+        Collections.sort(customers, new EmailComparator());
+        System.out.println(customers);
 
 //        System.out.println(Utils.max(new User(12), new User(40)));
 //        Utils.print("integer", 10);
--- src/com/wcoding/collections/ListDemo.java
+++ src/com/wcoding/collections/ListDemo.java
@@ -6,5 +6,14 @@ import java.util.List;
 public class ListDemo {
     public static void show(){
         List<String> list = new ArrayList<>();
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add(0, "!");
+
+        System.out.println(list.set(0, "+"));
+        System.out.println(list.get(0));
+        System.out.println(list);
     }
 }
